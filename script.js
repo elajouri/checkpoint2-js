2ec5c919d60118765578fa4640f24b2747781df8
Fabrizio El Ajouri Romano <fabrizio.el@gmail.com>
2023-05-08 21:48:03 -0300

    second checkpoint - web development with JavaScript

---
+++ script.js
@@ -0,0 +1,19 @@
+let input = document.querySelector('input')
+let button = document.querySelector('button')
+
+function verificaNota(){
+    if(input.value >= 0 && input.value <= 10){
+        if(input.value <= 5){
+            alert("reprovado");
+        } else if (input.value < 6){
+            alert("exame");
+        } else {
+            alert("aprovado!");
+        }
+    } else {
+        input.value= "";
+        alert("nota inválida!");
+    }
+}
+
+button.onclick = verificaNota;
